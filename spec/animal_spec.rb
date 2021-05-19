5f24ff5f1b17dfe51ba9ebb8a31a183caecca47f
Taylor Varoglu <tava8398@colorado.edu>
2021-05-19 14:42:51 -0600

    Add completed iteration 1

--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require './lib/animal'
+require_relative '../lib/animal'
 
 RSpec.describe Animal do
   it 'exists' do
@@ -30,5 +30,17 @@ RSpec.describe Animal do
     animal = Animal.new("Sea Otter", 10, 25)
 
     expect(animal.age_in_days).to eq(175)
+  end
+
+  it 'can be fed' do
+    animal = Animal.new("Sea Otter", 10, 25)
+
+    animal.feed!(3)
+
+    expect(animal.weight).to eq("13 pounds")
+
+    animal.feed!(2)
+
+    expect(animal.weight).to eq("15 pounds")
   end
 end
